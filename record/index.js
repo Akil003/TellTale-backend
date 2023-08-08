e71213d61f23ab4d176346dde40b522238546b7d
unknown <akilraj.subedi2020@vitstudent.ac.in>
2023-08-08 21:38:30 +0530

    Record audio books

--- record/index.js
+++ record/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router()
 const connection = require('../utils/connection')
 
-router.post('/', async (req, res) => {
-    const [email, ebook] = req.body
+const jsonParser = require('body-parser').json();
+
+router.post('/', jsonParser, async (req, res) => {
+    const {email, ebook} = req.body
+    
     if (!email || !ebook) {
         return res.status(400).json({
             error: `No email or ebook provided`
@@ -12,6 +15,7 @@ router.post('/', async (req, res) => {
     const existingDocument = await db.collection('records').findOne({ _id: email });
 
     function send(msg){
+        console.log(msg)
         return res.status(200).json({
             message : msg
         })
@@ -19,13 +23,13 @@ router.post('/', async (req, res) => {
 
     if (existingDocument) {
         // Email exists, check if audiobook ID already exists
-        const audiobookExists = existingDocument.audiobooks.some(item => item.id === audiobook.id);
+        const audiobookExists = existingDocument.ebooks.some(item => item.id === ebook.id);
 
         if (!audiobookExists) {
             // Audiobook ID doesn't exist, add it to the array
             await db.collection('records').updateOne(
                 { _id: email },
-                { $push: { audiobooks: audiobook } }
+                { $push: { ebooks : ebook } }
             );
             return send('Audiobook added to existing email');
         } else {
@@ -35,7 +39,7 @@ router.post('/', async (req, res) => {
         // Email doesn't exist, create a new document
         await db.collection('records').insertOne({
             _id: email,
-            audiobooks: [audiobook]
+            ebooks: [ebook]
         });
         return send('New email and audiobook added');
     }
